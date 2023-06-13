9c4b5cfd8d7facc42aec54c2696520e0a21d61fe
Jonathan Foster <jonathan.bruce.foster@gmail.com>
2023-06-12 21:16:16 -0400

    Fix basic test

---
+++ glue_heatmap/qt/tests/test_heatmap.py
@@ -0,0 +1,34 @@
+from ..data_viewer import HeatmapViewer
+from glue.app.qt import GlueApplication
+from glue.core.data import Data
+import numpy as np
+
+from glue_heatmap.coords import HeatmapCoordinates
+
+
+class TestSmallMultiplesViewer(object):
+    def setup_method(self, method):
+        self.app = GlueApplication()
+        self.session = self.app.session
+        self.hub = self.session.hub
+        self.data_collection = self.session.data_collection
+        values = np.array([[1, 2, 3], [4, 5, 6]])
+        strain_names = ["A", "B"]
+        marker_names = ["1", "2", "3"]
+        strain_array = np.array([strain_names for x in range(values.shape[1])]).T
+        marker_array = np.array([marker_names for x in range(values.shape[0])])
+        self.data = Data(
+            values=values,
+            x_cats=marker_array,
+            y_cats=strain_array,
+            coords=HeatmapCoordinates(
+                marker_names, strain_names, "Marker Name", "Parent Strain"
+            ),
+            label="test_data",
+        )
+        self.data_collection.append(self.data)
+        self.viewer = self.app.new_data_viewer(HeatmapViewer)
+        self.viewer.add_data(self.data)
+
+    def test_basic(self):
+        assert self.viewer is not None
