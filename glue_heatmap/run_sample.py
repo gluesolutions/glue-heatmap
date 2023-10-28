b5478cec621d701b1fa10081a46a29c2d4322987
Jonathan Foster <jonathan.bruce.foster@gmail.com>
2023-10-27 20:59:10 -0400

    Experiments to prevent subsets from passing though a Matrix

--- glue_heatmap/run_sample.py
+++ glue_heatmap/run_sample.py
@@ -1,11 +1,30 @@
 import pandas as pd
-# import numpy as np
 from glue.core import Data, DataCollection
+from glue_qt.viewers.scatter import ScatterViewer
 from glue.app.qt import GlueApplication
-from glue.core.link_helpers import LinkSame
+from glue.core.link_helpers import LinkSame, JoinLink
+from glue.core.joins import get_mask_with_key_joins
 
 from glue_heatmap.coords import HeatmapCoordinates
 from glue_heatmap.qt import HeatmapViewer
+from glue.core.exceptions import IncompatibleAttribute
+import traceback
+
+
+def norecurse(f):
+    def func(*args, **kwargs):
+        if len([x[2] for x in traceback.extract_stack() if x[2] == f.__name__]) > 0:
+            raise IncompatibleAttribute
+        return f(*args, **kwargs)
+    return func
+
+
+class MatrixData(Data):
+    def get_mask(self, subset_state, view=None):
+        try:
+            return subset_state.to_mask(self, view=view)
+        except IncompatibleAttribute:
+            return norecurse(get_mask_with_key_joins)(self, self._key_joins, subset_state, view=view)
 
 
 def demo():
@@ -16,7 +35,7 @@ def demo():
     values = qtl_matrix[strain_names].values.T
     marker_names = qtl_matrix["marker.id"].values
 
-    heatmap_data = Data(
+    heatmap_data = MatrixData(
         values=values,
         label="test",
         coords=HeatmapCoordinates(
@@ -30,15 +49,25 @@ def demo():
 
     ga = GlueApplication(dc)
 
-    marker_link = LinkSame(heatmap_data.id["Marker Name"], markers.id["markers"])
+    # Using JoinLinks on both rows and cols causes all subsets to propagate through the matrix
+    # which is not ideal. In real usage this is probably a DataAnnData object
+    marker_link = JoinLink(cids1=[heatmap_data.id["Marker Name"]], cids2=[markers.id["markers"]], data1=heatmap_data, data2=markers)
+    #marker_link = LinkSame(heatmap_data.id["Marker Name"], markers.id["markers"])
     dc.add_link(marker_link)
 
-    strain_link = LinkSame(heatmap_data.id["Parent Strain"], strains.id["strain"])
-
+    strain_link = JoinLink(cids1=[heatmap_data.id["Parent Strain"]], cids2=[strains.id["strain"]], data1=heatmap_data, data2=strains)
+    #strain_link = LinkSame(heatmap_data.id["Parent Strain"], strains.id["strain"])
     dc.add_link(strain_link)
 
     t = ga.new_data_viewer(HeatmapViewer)
     t.add_data(dc[0])
+
+    u = ga.new_data_viewer(ScatterViewer)
+    u.add_data(dc[1])
+
+    v = ga.new_data_viewer(ScatterViewer)
+    v.add_data(dc[2])
+
     ga.start()
 
 
