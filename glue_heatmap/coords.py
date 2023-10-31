f8cae0fee6e8259df88854d45d3cdef9ddd04881
Jonathan Foster <jonathan.bruce.foster@gmail.com>
2023-10-31 14:08:14 -0400

    Add save/restore for HeatmapCoordinates

--- glue_heatmap/coords.py
+++ glue_heatmap/coords.py
@@ -3,7 +3,7 @@ from glue.core.coordinates import IdentityCoordinates
 
 
 class HeatmapCoordinates(IdentityCoordinates):
-    def __init__(self, x_tick_names, y_tick_names, x_tick_label, y_tick_label):
+    def __init__(self, x_tick_names, y_tick_names, x_tick_label, y_tick_label, *args, **kwargs):
         super().__init__(n_dim=2)
         self._x_tick_names = np.array(x_tick_names)
         self._y_tick_names = np.array(y_tick_names)
@@ -28,3 +28,16 @@ class HeatmapCoordinates(IdentityCoordinates):
         # Returns an iterable of strings given the names of the world
         # coordinates for each axis.
         return [self._x_tick_label, self._y_tick_label]
+
+    def __gluestate__(self, context):
+        return dict(x_tick_names=context.do(self._x_tick_names),
+                    y_tick_names=context.do(self._y_tick_names),
+                    x_tick_label=self._x_tick_label,
+                    y_tick_label=self._y_tick_label)
+
+    @classmethod
+    def __setgluestate__(cls, rec, context):
+        return cls(x_tick_names=rec['x_tick_names'],
+                   y_tick_names=rec['y_tick_names'],
+                   x_tick_label=rec['x_tick_label'],
+                   y_tick_label=rec['y_tick_label'])
